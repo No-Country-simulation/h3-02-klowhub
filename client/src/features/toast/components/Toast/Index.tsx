84a450090c0f27f1dce0f035016065e4a514358d
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-11-13 08:51:33 -0300

    style: add docs for toast implementation

--- client/src/features/toast/components/Toast/Index.tsx
+++ client/src/features/toast/components/Toast/Index.tsx
@@ -6,6 +6,8 @@ import ToastProvider from './ToastProvider';
 import ToastTitle from './ToastTitle';
 import ToastViewport from './ToastViewport';
 
+//Implementacion: https://www.radix-ui.com/primitives/docs/components/toast
+//Implementacion: https://ui.shadcn.com/docs/components/toast
 export {
   Toast,
   ToastAction,
