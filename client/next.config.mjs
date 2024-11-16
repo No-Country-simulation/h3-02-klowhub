014601a6bbaf7382c64a847d9291b814c204fb05
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-11-15 21:05:15 -0300

    ci: fix dockerfile and workflow for env variables

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,18 +1,25 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const withNextIntl = createNextIntlPlugin('./src/core/lib/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const config = {
-    eslint: {
-        ignoreDuringBuilds: true,
-      },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   logging: {
     fetches: {
       fullUrl: true,
     },
-  }
+  },
+  output:"standalone",
+  outputFileTracingRoot: path.join(__dirname, ".next"),
 };
 
 export default withNextIntl(config);
