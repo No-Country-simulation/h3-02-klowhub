04c9ae2e2259786db23b48dc7a4b2ca6cf46cc7f
Cesar <117487764+sibas1@users.noreply.github.com>
2024-11-18 16:01:27 -0500

    feat: latest_inquiries

---
+++ client/src/features/home/HeadInquiries.tsx
@@ -0,0 +1,24 @@
+import { useTranslations } from 'next-intl';
+import styles from './HeadInquiries.module.css';
+
+export default function CardInquiries() {
+  const t = useTranslations('Platform');
+  return (
+    <div className={`${styles.gridContainer} max-w-[400px] rounded-lg bg-white/10 shadow-md`}>
+      <div className={`${styles.consulta} `}>
+        <p>{t('metainquiries')}</p>
+      </div>
+      <div className={`${styles.autor} `}></div>
+      <p>{t('metaauthor')}</p>
+      <div className={`${styles.fecha} `}>
+        <p>{t('metadate')}</p>
+      </div>
+      <div className={`${styles.plataforma} `}>
+        <p>{t('metaplatform')}</p>
+      </div>
+      <div className={`${styles.estado} `}>
+        <p>{t('metastad')}</p>
+      </div>
+    </div>
+  );
+}
