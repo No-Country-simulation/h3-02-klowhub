c7b2ae899378d9d1eb25be50f4d5530de59c1bb9
Cesar <117487764+sibas1@users.noreply.github.com>
2024-11-19 21:14:03 -0500

    feat(rutasGateway): se comenzo a crear alguna rutas en la gatewey y algunos dto

--- server/apps/gateway/src/auth/auth.controllers.ts
+++ server/apps/gateway/src/auth/auth.controllers.ts
@@ -1,19 +1,23 @@
 import {
+  Request,
   Controller,
   Post,
   Body,
   Inject,
   BadRequestException,
+  Patch,
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
 import { RegisterSchema } from './dto/registerSchema.dto';
+import { UpdateSchema } from './dto/updateSchema.dto';
+import { LoginSchema } from './dto/loginSchema.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     @Inject('AUTH_MICROSERVICE') private readonly authClient: ClientProxy,
-  ) {}
+  ) { }
 
   @Post('register')
   async register(@Body() registerDto: any): Promise<any> {
@@ -26,6 +30,44 @@ export class AuthController {
     // Enviar los datos al microservicio y manejar el Observable como una Promesa
     return lastValueFrom(
       this.authClient.send({ cmd: 'register' }, registerDto),
+    );
+  }
+
+  @Post('verifyEmail')
+  async verifyEmail(@Body('token') token: string): Promise<any> {
+    return lastValueFrom(
+      this.authClient.send({ cmd: 'verifyEmail' }, { token }),
+    );
+  }
+  @Post('google')
+  async google(@Body() token: string): Promise<any> {
+    return lastValueFrom(
+      this.authClient.send({ cmd: 'google' }, { token }),
+    );
+  }
+  @Patch('update')
+  async update(@Body() updateDto: any, @Request() req: any): Promise<any> {
+    const token = req.cookies.token;
+    if (!token) {
+      throw new BadRequestException('Token not found in cookies');
+    }
+    const validationResult = UpdateSchema.safeParse(updateDto);
+    if (!validationResult.success) {
+      throw new BadRequestException(validationResult.error.errors);
+    }
+    return lastValueFrom(
+      this.authClient.send({ cmd: 'google' }, { token, updateDto }),
+    );
+  }
+  
+  @Post('login')
+  async login(@Body() loginDto: any): Promise<any> {
+    const validationResult = LoginSchema.safeParse(loginDto);
+    if (!validationResult.success) {
+      throw new BadRequestException(validationResult.error.errors);
+    }
+    return lastValueFrom(
+      this.authClient.send({ cmd: 'login' }, loginDto),
     );
   }
 }
