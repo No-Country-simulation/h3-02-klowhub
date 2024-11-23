f07543cfee3c794546b6e0eaa79b5e0d6f4d6445
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-11-23 00:08:08 -0300

    fix: .env gateway, users , update middeware

--- server/apps/gateway/src/main.ts
+++ server/apps/gateway/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(new AuthMiddleware().use);
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Configuración de CORS
+    origin: process.env.FRONTEND_URL || 'http://frontend@example', // Configuración de CORS
     credentials: true, // Permitir cookies
   });
   await app.listen(process.env.PORT || 3000);
