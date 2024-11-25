6ae6af6a046d3336bc3dddf69a3263b10a7d2307
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-11-24 21:03:56 -0300

    fix: fixe add join-path in proto paths

--- server/apps/gateway/src/pubsub/pubsub.module.ts
+++ server/apps/gateway/src/pubsub/pubsub.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PubSubGatewayController } from './pubsub.gateway.controller';
 import { PubSubGatewayService } from './pubsub.gateway.service';
+import { join } from "path";
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { PubSubGatewayService } from './pubsub.gateway.service';
         transport: Transport.GRPC,
         options: {
           package: 'googlecloudstorage', 
-          protoPath: '../../protos/upload.proto', 
+          protoPath: join(__dirname,'../../protos/upload.proto'),
           url: `${process.env.UPLOAD_MICROSERVICE_HOST || "0.0.0.0"}:${process.env.UPLOAD_SERVICE_PORT || 3003}`,
         },
       },
