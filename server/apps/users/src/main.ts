39585add3fb87e88584d60547175738ce3e88f95
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-11-25 15:45:05 -0300

    fix: role emun

--- server/apps/users/src/main.ts
+++ server/apps/users/src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   );
 
   await app.listen();
-  console.log('Microservice is listening...');
+  console.log('Microservice Users is listening...', process.env.USERS_SERVICE_PORT);
 }
 bootstrap();
