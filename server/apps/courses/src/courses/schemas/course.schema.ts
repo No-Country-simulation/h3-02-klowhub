3fe64102924df9a0d0b34cc69d8629b4402b82bf
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-11-26 19:03:15 -0300

    update shema courses users in mongodb

--- server/apps/courses/src/courses/schemas/course.schema.ts
+++ server/apps/courses/src/courses/schemas/course.schema.ts
@@ -1,7 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { Module, ModuleSchema } from './module.schema';
 import { MergeInfo, MergeInfoSchema } from './merge-info.schema';
+import { Funtionalidad, Idiom, Pilar, Platform, Sector, Tool } from './enums';
 
 @Schema()
 export class Course extends Document {
@@ -21,20 +22,63 @@ export class Course extends Document {
 
   @Prop({
     type: String,
-    enum: ['appsheet', 'powerapps'],
-    required: true,
-  })
-  courseType: string; // Tipo de curso
-
-  @Prop({
-    type: String,
     enum: ['course', 'lesson'],
-    required: true,
   })
   kind: string; // Curso o lección
 
   @Prop()
   basicDescription?: string; // Descripción básica del curso
+
+  @Prop()
+  type: string;
+  enum : ['basic', 'intermed']
+
+  @Prop({
+    type: String,
+    enum: Platform,
+    default: Platform.APPSHEET,
+  })
+  platafor: string;
+
+  @Prop({
+    type: String,
+    enum: Idiom,
+    default: Idiom.SPANISH,
+  })
+  idiom: string;
+
+  @Prop({
+    type: String,
+    enum: Pilar,
+    default: '',
+  })
+  pilar: string;
+
+  @Prop({
+    type: String,
+    enum: Funtionalidad,
+    default: Funtionalidad.EMAILS,
+  })
+  funtionalidad: string;
+
+  @Prop({
+    type: String,
+    enum: Sector,
+    default: Sector.INDUSTRIA,
+  })
+  sector :string;
+
+  @Prop({
+    type: String,
+    enum: Tool,
+    default: Tool.AIRTABLE,
+  })
+  tool : string;
+
+  // detalles del curso
+
+  @Prop()
+  descriptionBasic? : string // descripcion basica
 
   @Prop({ type: [String] })
   prerequisites?: string[]; // Requisitos previos del curso
@@ -45,6 +89,8 @@ export class Course extends Document {
   @Prop()
   imageUrl?: string; // URL de la imagen del curso
 
+  // tercer paso 
+
   @Prop({ type: [ModuleSchema], default: [] })
   modules?: Module[]; // Lista de módulos
 
@@ -53,6 +99,10 @@ export class Course extends Document {
 
   @Prop({ default: 'in-progress' })
   status: string; // Estado del curso
+
+  // listado de usuario que estan en este curso
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Users' }], default: [] })
+  enrolledUsers: Types.ObjectId[]; // Usuarios inscritos en el curso
 
   @Prop({ default: Date.now })
   createdAt: Date; // Fecha de creación
