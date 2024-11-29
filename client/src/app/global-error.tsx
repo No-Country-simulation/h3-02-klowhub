37b3b845fa46b0bc9abc5c9dc3ea3873235df102
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-11-29 18:49:15 -0300

    fix: fixe global error

--- client/src/app/global-error.tsx
+++ client/src/app/global-error.tsx
@@ -1,12 +1,11 @@
-'use client';
-
-export default function GlobalError(props: {
+export default async function GlobalError(props: {
   error: Error & { digest?: string };
   reset: () => void;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
+  const { locale } = await props.params;
   return (
-    <html lang={props.params.locale}>
+    <html lang={locale}>
       <body>
         {/* Pagina de errores global. Modificar estilo o eliminar si es necesario*/}
         <h2>Something went wrong!</h2>
