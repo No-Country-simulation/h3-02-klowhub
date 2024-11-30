9ada5fd00277efac1c19e2806823c2b936a1198a
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-11-30 14:30:47 -0300

    fix: update users service lock file

--- server/apps/users/src/main.ts
+++ server/apps/users/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
-console.log('Env variables:', process.env);
+
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
