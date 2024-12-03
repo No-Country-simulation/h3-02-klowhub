552d5fde22e0bf6f9ac944b3714237b142e7db79
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-12-03 07:45:10 -0300

    fix: remove static imports

--- client/src/features/creator/components/ContentDropdown/Index.tsx
+++ client/src/features/creator/components/ContentDropdown/Index.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 import type { Dispatch, SetStateAction } from 'react';
+import Button from '@core/components/Button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@core/components/Dropdown/Index';
-import Button from '@core/components/Button';
 import TabsTrigger from '@core/components/Tabs/TabTrigger';
-import caretDown from '/public/svg/caretDown.svg';
 
 interface ContentDropdownProps {
   general: string;
@@ -43,7 +42,13 @@ const ContentDropdown = ({
         <DropdownMenuTrigger asChild>
           <Button variant="outline" className="w-full justify-between">
             {activeTab}
-            <Image src={caretDown} alt="caretDown" width={16} height={16} className="ml-2 size-4" />
+            <Image
+              src={'/svg/caretDown.svg'}
+              alt="Caret down"
+              width={16}
+              height={16}
+              className="ml-2 size-4"
+            />
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-[var(--radix-popper-anchor-width)] bg-neutral-100/50 backdrop-blur-lg min-[580px]:hidden">
