06b69d4f8f32967192c651554e4c4bdf59f6697e
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-12-03 07:11:12 -0300

    fix: fixe mobile navbar

--- client/src/core/components/NavLink/index.tsx
+++ client/src/core/components/NavLink/index.tsx
@@ -7,9 +7,10 @@ import { cn } from '../../lib/utils';
 interface NavLinkProps {
   href: string;
   children: ReactNode;
+  className?: string;
 }
 
-const NavLink = ({ href, children }: NavLinkProps) => {
+const NavLink = ({ href, children, className = '' }: NavLinkProps) => {
   const pathname = usePathname();
   const isActive = pathname === href;
 
@@ -17,8 +18,9 @@ const NavLink = ({ href, children }: NavLinkProps) => {
     <Link
       href={href}
       className={cn(
-        'text-base font-bold leading-5 transition-colors ease-in hover:text-primary-A-200',
-        isActive ? 'text-primary-B-200' : 'text-primary-B-100'
+        'text-base font-semibold leading-5 transition-colors ease-in hover:text-primary-A-200',
+        isActive ? 'text-primary-B-200' : 'text-primary-B-100',
+        className
       )}>
       {children}
     </Link>
