8c57f14367729306a5635c30a992a5d7ca9a7b96
Moya30 <moyafp21@gmail.com>
2024-12-09 20:30:24 -0500

    fix: en construcción proyectos / consultas / sobre appshet

--- client/src/app/[locale]/(auth)/consultancies/page.tsx
+++ client/src/app/[locale]/(auth)/consultancies/page.tsx
@@ -1,4 +1,5 @@
 import { getTranslations, setRequestLocale } from 'next-intl/server';
+import ComingSoon from '@core/components/ComingSoon';
 import { routing } from '@core/lib/i18nRouting';
 
 export async function generateStaticParams() {
@@ -33,6 +34,14 @@ export default async function ConsultanciesPage({
   params,
 }: Readonly<{ params: Promise<{ locale: string }> }>) {
   const { locale } = await params;
+  const ct = await getTranslations<'ComingSoon'>({
+    locale: locale,
+    namespace: 'ComingSoon',
+  });
   setRequestLocale(locale);
-  return <main className="mt-36 size-full px-10 sm:px-[51px] min-[1800px]:px-16"></main>;
+  return (
+    <main className="mt-36 size-full px-10 sm:px-[51px] min-[1800px]:px-16">
+      <ComingSoon className="self-center" title={ct('title')} subTitle={ct('subTitle')} />
+    </main>
+  );
 }
