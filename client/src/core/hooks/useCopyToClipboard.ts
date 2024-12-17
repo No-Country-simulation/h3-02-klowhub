01b365c7801c8cc12a4859ab62d0769ae9e6008b
Cesar <117487764+sibas1@users.noreply.github.com>
2024-12-17 15:13:00 -0500

    fix(rut): user/:id

--- client/src/core/hooks/useCopyToClipboard.ts
+++ client/src/core/hooks/useCopyToClipboard.ts
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
 import { fallbackCopyTextToClipboard } from '@core/services/fallbackCopyTextToClipboard';
-import { NODE_ENV } from "@root/env.config";
+import env from '@root/env.config';
 import useRefMounted from './useRefMounted';
 import useTimeout from './useTimeout';
 import useToggle from './useTogle';
@@ -18,7 +18,7 @@ export const useCopyToClipboard = () => {
         console.warn('No value provided to copy');
         return false;
       }
-      if (NODE_ENV === 'development' && typeof value !== 'string') {
+      if (env.NODE_ENV === 'development' && typeof value !== 'string') {
         console.error(`Cannot copy typeof ${typeof value} to clipboard, must be a string`);
         return;
       }
